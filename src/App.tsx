d23761c9f4436dd7f3e97e0ba4b0c9827d2572de
NicklasSoderlund <nicklassoderlund96@gmail.com>
2023-03-08 14:44:41 +0100

    Initiated project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { createResturant } from './Services/createResturant';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { StartPage } from './components/StartPage/Startpage';
+import { Booking } from './components/Booking/Booking';
+import { Contact } from './components/Contact/Contact';
+import { Admin } from './components/Admin/Admin';
+
+const apiId = "64088bb976187b915f68e167";
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<StartPage></StartPage>}></Route>
+      <Route path="/booking" element={<Booking></Booking>}></Route>
+      <Route path="/contact" element={<Contact></Contact>}></Route>
+      <Route path="/admin" element={<Admin></Admin>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
