6e9957d123a05028cec5caa07e6135001b2dc910
Giovanni <giobyw@gmail.com>
2023-03-09 02:40:41 +0100

    Form taking form

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -17,7 +17,7 @@ function BookingForm(props: BookingFormProps): JSX.Element {
   return (
     <section className='Booking'>
       <h2>Book a table!</h2>
-      <h3>Please notice that we only offer two different sittings per day. </h3>
+      <h3>Please notice that you can only book up to 6 person per reservation.</h3>
     <div className='FormContainer'>
     <form onSubmit={handleSubmit}>
       <label htmlFor="name">Namn:</label>
@@ -28,12 +28,25 @@ function BookingForm(props: BookingFormProps): JSX.Element {
 
       <label htmlFor="date">Datum:</label>
       <input type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+      <div className='timeContainer'>
+      <label htmlFor="numPeople">Antal gäster:</label>
+      <input type="number" id="numPeople" value={numPeople} onChange={(e) => {
+          const value = Number(e.target.value);
+          if (value >= 1 && value <= 6) {
+          setNumPeople(value);
+    }
+  }} 
+  required 
+  min="1" 
+  max="6"
+/>
 
       <label htmlFor="time">Tid:</label>
-      <input type="time" id="time" value={time} onChange={(e) => setTime(e.target.value)} required />
-
-      <label htmlFor="numPeople">Antal gäster:</label>
-      <input type="number" id="numPeople" value={numPeople} onChange={(e) => setNumPeople(Number(e.target.value))} required />
+      <input type="radio" id="time1" name="time" value="18:00" checked={time === "18:00"} onChange={(e) => setTime(e.target.value)} />
+      <label htmlFor="time1">18:00</label>
+      <input type="radio" id="time2" name="time" value="21:00" checked={time === "21:00"} onChange={(e) => setTime(e.target.value)} />
+      <label htmlFor="time2">21:00</label>
+      </div>
       <br></br>
       <button type="submit">Book a table</button>
     </form>
