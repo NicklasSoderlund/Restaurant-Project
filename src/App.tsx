1c3939e627082babca7c11b5375a465f531addef
Giovanni <giobyw@gmail.com>
2023-03-09 01:06:34 +0100

    Started form

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { StartPage } from './components/StartPage/Startpage';
 import { Booking } from './components/Booking/Booking';
 import { Contact } from './components/Contact/Contact';
 import { Admin } from './components/Admin/Admin';
+import { Nav } from './components/Nav/Nav';
+import { Footer } from './components/Footer/Footer';
 
 const apiId = "64088bb976187b915f68e167";
 
@@ -14,14 +16,17 @@ function App() {
 
   
   return (
-     
+
     <BrowserRouter>
+        <Nav></Nav>
     <Routes>
       <Route path="/" element={<StartPage></StartPage>}></Route>
       <Route path="/booking" element={<Booking></Booking>}></Route>
       <Route path="/contact" element={<Contact></Contact>}></Route>
       <Route path="/admin" element={<Admin></Admin>}></Route>
+      <Route path="*" element={<h3>Page not found</h3>}></Route>
       </Routes>
+      <Footer></Footer>
     </BrowserRouter>
   );
 }
--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -1,6 +1,42 @@
-export function Booking() {
-   
-    return (
-        <div></div>
-    )
+import React, { useState } from 'react';
+import './booking.scss';
+
+interface BookingFormProps {}
+
+function BookingForm(props: BookingFormProps): JSX.Element {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [numPeople, setNumPeople] = useState(1);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Handle form submission here
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+
+      <label htmlFor="date">Date:</label>
+      <input type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+
+      <label htmlFor="time">Time:</label>
+      <input type="time" id="time" value={time} onChange={(e) => setTime(e.target.value)} required />
+
+      <label htmlFor="numPeople">Number of people:</label>
+      <input type="number" id="numPeople" value={numPeople} onChange={(e) => setNumPeople(Number(e.target.value))} required />
+
+      <button type="submit">Book table</button>
+    </form>
+  );
+}
+
+export function Booking(): JSX.Element {
+  return <BookingForm />;
 }
