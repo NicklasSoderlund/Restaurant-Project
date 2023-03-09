58f6939511a3a6abc1f832be7b57466302321e89
NicklasSoderlund <nicklassoderlund96@gmail.com>
2023-03-09 20:42:29 +0100

    fixed bookingscontext

--- src/Services/createResturant.ts
+++ src/Services/createResturant.ts
@@ -16,3 +16,4 @@ return console.log(myId);
 
 
 }
+
---
+++ src/Services/fetchBookings.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export interface IBooking {
+    id: string,
+    date: string,
+    time: string,
+    numberOfGuests: number,
+    customerId: string
+}
+
+
+
+export async function fetchBookings(){
+
+    let response = await axios.get('https://school-restaurant-api.azurewebsites.net/booking/restaurant/64088bb976187b915f68e167');
+    return response.data as IBooking[];
+    
+    }
